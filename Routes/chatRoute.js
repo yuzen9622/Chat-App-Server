97ba2d3e9cbd700cf49c7a3b7da5fcc301a79ce5
oscar <oscar48079@gmail.com>
2024-02-16 19:11:25 +0800

    update chat-delete

--- Routes/chatRoute.js
+++ Routes/chatRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createChat, findUserChats, findChats } = require("../Controllers/chatConsle");
+const { createChat, findUserChats, findChats, delChat } = require("../Controllers/chatConsle");
 
 const router = express.Router();
 
 router.post("/", createChat)
 router.get("/:userId", findUserChats)
 router.get("/find/:firstId/:secondId", findChats);
+router.get("/delete/:userId/:secondId", delChat);
 module.exports = router;
