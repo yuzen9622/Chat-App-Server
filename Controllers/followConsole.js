37580101d6d864ca4ddc08ba489e7620a772cb08
oscar <oscar48079@gmail.com>
2024-02-26 16:48:55 +0800

    add friend system

---
+++ Controllers/followConsole.js
@@ -0,0 +1,47 @@
+const followModel = require('../Database/followData');
+const chatModel=require('../Database/chatData')
+const msgModel=require('../Database/chatData')
+const createFriend=async(req,res)=>{
+    const { firstId, secondId } = req.body;
+    try {
+        const newFriend=new followModel({
+            members:[firstId,secondId]
+        });
+        const response = await newFriend.save()
+res.status(200).json(response);
+    } catch (error) {
+        
+    }
+}
+const deleteFriend=async(req,res)=>{
+    const { firstId, secondId } = req.body;
+    try {
+        const delfriend = await followModel.findOneAndDelete({
+            members: { $all: [firstId, secondId] }
+        })
+        const delchat = await chatModel.findOneAndDelete({
+            members: { $all: [firstId, secondId] }
+        })
+        const friend=await followModel.find({
+            members:{$all:[firstId]}
+        })
+        
+        const chatId = delchat._id
+        await msgModel.deleteMany({ chatId: chatId })
+       
+        res.status(200).json(friend)
+    }catch(err){
+        res.status(200).json(err)
+    }
+}
+
+const findFriend=async(req,res)=>{
+    const {userId}=req.params;
+    const friend=await followModel.find({
+        members:{$all:[userId]}
+    })
+    res.status(200).json(friend)
+
+}
+
+module.exports={createFriend,deleteFriend,findFriend}
