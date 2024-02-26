7031b4edc72fc22ce7e84c12e3868eb11fe88ce2
oscar <oscar48079@gmail.com>
2024-02-26 16:50:08 +0800

    add friend system

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute")
 const chatRoute = require("./Routes/chatRoute")
 const msgRoute = require("./Routes/msgRoute")
+const friendRoute=require('./Routes/friendRoute')
 const fileupload = require('express-fileupload')
+
 const app = express();
 require("dotenv").config()
 
@@ -13,6 +15,7 @@ app.use(cors())
 app.use(fileupload()).use("/users", userRoute)
 app.use("/chat", chatRoute)
 app.use("/msg", msgRoute)
+app.use('/friend',friendRoute)
 app.use("/upload", express.static('upload'))
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
